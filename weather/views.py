28bfdf5052ad15cb41a27e3ee78c67c5a5ed8845
Arthur Brophy <artbrophy@gmail.com>
2018-08-23 16:22:28 +0200

    add forecast index view
    base layout added with bootstrap boilerplate

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Forecast
+
+app_name = 'weather'
 
 def index(request):
-    render(request, 'weather/index.html')
+    forecasts = Forecast.objects.all()
+    context = {
+        'forecasts': forecasts
+    }
+    return render(request, 'weather/index.html', context)
